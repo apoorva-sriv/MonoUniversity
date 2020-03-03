028a7a5b13610d091b91fd8535640a019c3f07dc
LittleBadis <badis-xbox@live.com>
2020-03-03 12:02:37 -0500

    Added functional bare bones game logic.
    4 players, 3 AI, 1 human, player order is chosen at random. Human player is admin and can "kick" AI players or resign.
    AI will roll their dice and move, player will have to press the RTD button upon his turn.
    Check console for this, no visuals implemented as of yet.

--- script/board.js
+++ script/board.js
@@ -6,6 +6,7 @@ let login = null;
 
 // Some constants needed to construct the board
 const maxTiles = 40;
+const maxDoubles = 3;
 const unbuyableTiles = [0, 2, 4, 7, 10, 17, 20, 22, 30, 33, 36, 38];
 const chanceTiles = [7, 22, 36];
 const communityTiles = [2, 17, 33];
@@ -32,6 +33,17 @@ const TILE_FLAG_GOTOJAIL = 32;
 const TILE_FLAG_TAX = 64;
 const TILE_FLAG_UTILITY = 128;
 const TILE_FLAG_TTC = 256; 
+
+// Game State Flags
+const GAMESTATE_END = 0;
+const GAMESTATE_PLAYER_TURN = 1;
+const GAMESTATE_PLAYER_ROLL = 2;
+const GAMESTATE_PLAYER_MOVE = 4;
+const GAMESTATE_PLAYER_DECISION = 8;
+const GAMESTATE_AI_TURN = 16;
+const GAMESTATE_AI_ROLL = 32;
+const GAMESTATE_AI_MOVE = 64;
+const GAMESTATE_AI_DECISION = 128;
 
 // Building codes, building names and descriptions are stored here for convenience
 const tileNames = [ ["GO", "GO",],
@@ -103,7 +115,8 @@ function playerClass()
 	this.piece = null,
 	this.color = null,
 	this.money = 1500,
-	this.jailed = false
+	this.jailed = false,
+	this.position = 0
 };
 
 // This is board class, it contains the tiles, a tracker of player turn, and in which order players play. The game state is also saved here
@@ -113,7 +126,8 @@ function boardClass()
 	this.players = [],
 	this.playerTurns = [],
 	this.playerTurn = 0,
-	this.gameState = 0
+	this.gameState = 0,
+	this.dice = [ 1, 1 ]
 }
 
 // This is where all information pertinent to a tile is stored
@@ -135,6 +149,8 @@ function tileClass()
 	this.buildings = 0
 }
 
+window.addEventListener('load', readyBoard);
+
 // This function populates the board with a tile
 function initialzeBoard(board)
 {
@@ -147,7 +163,7 @@ function initialzeBoard(board)
 		newTile.fullname = tileNames[i][1];
 		//newTile.desc = 
 		newTile.image = newTile.name + ".png";
-			
+		
 		// Check if those are corner tiles and apply the necessary properties
 		if (cornerTiles.includes(i))
 		{
@@ -211,6 +227,30 @@ function initialzeBoard(board)
 		
 		
 		board.tiles.push(newTile);
+		
+		// Set the names
+		const boardHTML = document.getElementById('board');
+		const boardHTMLTile = boardHTML.children[i + 1];
+		if (cornerTiles.includes(i))
+		{
+			boardHTMLTile.innerHTML = newTile.name;
+		}
+		else if (communityTiles.includes(i) || chanceTiles.includes(i))
+		{
+			boardHTMLTile.children[0].innerHTML = newTile.name;
+		}
+		else if (utilityTiles.includes(i) || ttcTiles.includes(i) || taxTiles.includes(i))
+		{
+			boardHTMLTile.children[0].innerHTML = newTile.name;
+			boardHTMLTile.children[1].innerHTML = '$' + newTile.price;
+		}
+		else
+		{
+			boardHTMLTile.children[1].innerHTML = newTile.name;
+			boardHTMLTile.children[2].innerHTML = '$' + newTile.price;
+		}
+		
+		// Set the prices
 	}
 }
 
@@ -226,33 +266,92 @@ function initializePlayers(board, numPlayers)
 	if (!login)
 		alert("No user logged in");
 	
+	// Add actual player
 	const newPlayer = new playerClass()
 	newPlayer.user = login;
-	board.players.push(newPlayer)
+	board.players.push(newPlayer);
+	board.playerTurns.push(0);
 	
+	// Add AI players
 	for (let i = 1; i < numPlayers; i++)
 	{
 		const newAI = new playerClass();
 		board.players.push(newAI)
+		board.playerTurns.push(i);
+	}
+	
+	// Shuffle playing order
+	board.playerTurns.sort(() => Math.random() - 0.5);
+	console.log(board.playerTurns);
+	
+	if (board.players[board.playerTurns[0]].user == null)
+	{
+		board.gameState = GAMESTATE_AI_TURN;
+		setTimeout(aiRollTheDice, 2000);
+	}
+	else
+	{
+		board.gameState = GAMESTATE_PLAYER_TURN;
+	}
+	
+	console.log('Player ' + board.playerTurns[board.playerTurn] + "'s turn ");
+}
+
+// Get the player list prepared. Add a resign button for human player and highlight whoever's turn it is, if human is logged as admin, he can kick other players out
+function initializePlayerList(board)
+{
+	const playerList = document.getElementById("playerList");
+	
+	// Add the players in descending order of the playerturn list	
+	for (let i = 0; i < board.players.length; i++)
+	{
+		const actualPlayerId = board.playerTurns[i];
+		const playerSlot = document.createElement("div");
+		playerSlot.setAttribute("id","playerSlot");
+		const playerSlotText = document.createTextNode("Player " + actualPlayerId);
+		playerSlot.appendChild(playerSlotText);
+		
+		if (actualPlayerId == 0)
+		{
+			const playerResignButton = document.createElement("button");
+			playerResignButton.setAttribute("id","resignButton");
+			playerResignButton.setAttribute("onclick","playerResign(event)");
+			const playerResignButtonText = document.createTextNode("RESIGN");
+			playerResignButton.appendChild(playerResignButtonText);
+			playerSlot.appendChild(playerResignButton);
+		}
+		else
+		{
+			if (login.isAdmin)
+			{
+				const playerKickButton = document.createElement("button");
+				playerKickButton.setAttribute("id","kickButton");
+				playerKickButton.setAttribute("onclick","playerKick(event)");
+				const playerKickButtonText = document.createTextNode("KICK");	
+				playerKickButton.appendChild(playerKickButtonText);
+				playerSlot.appendChild(playerKickButton);
+			}
+		}
+		
+		playerList.appendChild(playerSlot);
 	}
 }
 
 // Test to initialize users
 const testUser = new userClass("admin", "admin", 1);
 testUser.isAdmin = true;
-login = testUser;
 const testUser2 = new userClass("user", "user", 2);
-
+login = testUser;
 
 // Create an instance of the gameboard, this is essentially where most of the game takes place and where most of the vital information is also kept
 const gameBoard = new boardClass();
-initialzeBoard(gameBoard);
 
-// Placeholder feature: Clicking on a tile will generate its information on the property information section. Note that not all tiles are properties, but we can still retrieve information from them.
-const tile = document.getElementById('tile');
-if (tile)
+// Ready the board for testing
+function readyBoard()
 {
-	tile.addEventListener('click', parseInfo)
+	initialzeBoard(gameBoard);
+	initializePlayers(gameBoard, 4);
+	initializePlayerList(gameBoard);
 }
 
 // Tile information displayed on propertyInfo
@@ -282,7 +381,7 @@ function parseInfo(e)
 	
 	// Setup
 	const tile = e.target;
-	const boardTile = document.getElementsByClassName("board")[0];
+	const boardTile = document.getElementById("board");
 	let tileInfo = null;
 	let index = 1;
 
@@ -300,7 +399,7 @@ function parseInfo(e)
 	if (tileInfo)
 	{
 		// Get element and purge its inner contents
-		const infoTile = document.getElementsByClassName("propertyInfo")[0];
+		const infoTile = document.getElementById("propertyInfo");
 		infoTile.innerHTML = "";
 		
 		// Set the header
@@ -339,5 +438,108 @@ function parseInfo(e)
 		infoTile.appendChild(infoTileText);
 		
 	}
+}
 
+// Event handler for human player
+function playerRollTheDice(e)
+{
+	// Prevent Default
+	e.preventDefault();
+	
+	// If it's not the player's turn do not roll the dice
+	if (gameBoard.gameState != GAMESTATE_PLAYER_TURN)
+		return;
+	
+	// Immediately roll the dice for the player
+	rollTheDice();
+	
+}
+
+// AI function for rolling the dice
+function aiRollTheDice()
+{
+	// If it's not the player's turn do not roll the dice
+	if (gameBoard.gameState != GAMESTATE_AI_TURN)
+		return;
+	
+	// Roll the dice
+	rollTheDice();
+}
+
+// Rolling the dice
+function rollTheDice()
+{
+	const currentPlayer = gameBoard.players[gameBoard.playerTurns[gameBoard.playerTurn]];
+	
+	if (currentPlayer.user == null)
+		gameBoard.gameState = GAMESTATE_AI_ROLL;
+	else
+		gameBoard.gameState = GAMESTATE_PLAYER_ROLL;
+	
+	// Dice rolling away
+	gameBoard.dice[0] = 1 + Math.floor(Math.random() * Math.floor(6));
+	gameBoard.dice[1] =  1 + Math.floor(Math.random() * Math.floor(6));
+	
+	console.log('Player ' + gameBoard.playerTurns[gameBoard.playerTurn] + ' has rolled ' + gameBoard.dice[0] + ' ' + gameBoard.dice[1]);
+	
+	// Delay player movement
+	setTimeout(playerMove, 2000);
+}
+
+// Moving the player
+function playerMove()
+{
+	const player = gameBoard.players[gameBoard.playerTurns[gameBoard.playerTurn]];
+	
+	if (player.position + gameBoard.dice[0] + gameBoard.dice[1] > maxTiles - 1)
+		player.position = player.position + gameBoard.dice[0] + gameBoard.dice[1] - 40;
+	else
+		player.position = player.position + gameBoard.dice[0] + gameBoard.dice[1];
+	
+	console.log('Player ' + gameBoard.playerTurns[gameBoard.playerTurn] + ' is now at position ' + player.position);
+	
+	// Hand the turn to the next player
+	setTimeout(nextTurn, 2000);
+}
+
+// Giving the dice to the next player
+function nextTurn()
+{
+	// Player keeps his turn if he lands double, so check before incrementing.
+	if (gameBoard.dice[0] != gameBoard.dice[1])
+	{
+		if (gameBoard.playerTurn + 1 >= gameBoard.players.length)
+			gameBoard.playerTurn = 0;
+		else
+			gameBoard.playerTurn += 1;
+	}
+	
+	// Check whose turn it is
+	if (gameBoard.players[gameBoard.playerTurns[gameBoard.playerTurn]].user == null)
+	{
+		gameBoard.gameState = GAMESTATE_AI_TURN;
+		setTimeout(aiRollTheDice, 2000);
+	}
+	else
+	{
+		gameBoard.gameState = GAMESTATE_PLAYER_TURN
+	}
+	console.log('Player ' + gameBoard.playerTurns[gameBoard.playerTurn] + "'s turn ");
+	
+}
+
+function playerKick(e)
+{
+	// Prevent default
+	e.preventDefault();
+	
+	console.log("Kick me.");
+}
+
+function playerResign(e)
+{
+	// Prevent default
+	e.preventDefault();
+	
+	console.log("Loser.");
 }
