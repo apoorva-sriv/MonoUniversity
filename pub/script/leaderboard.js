7a186acfec79204e7d5fca56e0d72010181c7406
LittleBadis <badis-xbox@live.com>
2020-04-03 14:35:44 -0400

    Added MangoDB interactions
    Added script to fetch and generate the top 5 users on the database.

---
+++ pub/script/leaderboard.js
@@ -0,0 +1,88 @@
+/* leaderboard js */
+
+"use strict";
+
+// We do not need to fetch the current user, all we need is to fetch the entire userbase, and display the thing
+
+// Very basic user function to make this happen
+function User(username, wins)
+{
+	this.username = username,
+	this.wins = wins;
+}
+
+// Function to add the user in question to the board
+function addLeaderboardMember(user)
+{
+	// Get info from param
+	const username = user.username;
+	const wins = user.wins;
+	
+	// Definitions
+	const leaderboardList = document.querySelector('#userList');
+	const tableRow = document.createElement("tr");
+	const tableUser = document.createElement("td");
+	const tableUserText = document.createTextNode(username);	
+	const tableWins = document.createElement("td");
+	const tableWinsText = document.createTextNode(wins);	
+	
+	// Additions
+	tableUser.appendChild(tableUserText);
+	tableWins.appendChild(tableWinsText);
+	tableRow.appendChild(tableUser);
+	tableRow.appendChild(tableWins);
+	leaderboardList.appendChild(tableRow);
+}
+
+// Compare function to sort the leaderboard list in descending order
+function sortLeaderboard(userA, userB)
+{
+	if (userA.wins > userB.wins) return -1;
+    if (userB.wins > userA.wins) return 1;
+    return 0;
+}
+
+// Function to fetch user information and apply everything we need for the leaderboard
+function startup()
+{
+    const url = '/api/users/all';
+
+    fetch(url)
+    .then((res) => { 
+        if (res.status === 200) {
+           return res.json() 
+       } else {
+            alert('Could not get users')
+       }                
+    })
+    .then((json) => {
+        json.map((user) => {
+			
+			// Initialize array
+			let userArray = [];
+			
+			// Get all the users and store them
+			for (let i = 0; i < user.length; i++)
+			{
+				const newUser = new User(user.user, user.wins);
+				userArray.push(newUser);
+			}
+			
+			// Sort the array
+			userArray.sort(sortLeaderBoard);
+			
+			// Generate the top 5 people
+			for (let i = 0; (i < userArray.length || i < 5); i++)
+			{
+				addLeaderboardMember(userArray[i]);
+			}
+			
+        })
+    }).catch((error) => {
+        log(error)
+    })
+
+}
+
+// Start it up to have it be done
+startup()
